a29082c02d01b83301570af3f6ddc845ad3f08de
IsaacDSC <isaac8.silva@hotmail.com>
2023-09-10 17:32:24 -0300

    fix: remove configs environments not used

--- config/environment.go
+++ config/environment.go
@@ -7,17 +7,10 @@ import (
 )
 
 type Environment struct {
-	AWS_ACCESS_KEY_ID      string
-	AWS_SECRET_ACCESS_KEY  string
-	DATABASE_URL           string
-	TOKEN_API              string
-	SECRET_API             string
-	GODEV                  string
-	DATABASE_URL_ANALYTICS string
-	PORT                   string
-	WHOAMI_URL_DEV         string
-	WHOAMI_URL_PROD        string
-	WHOAMI_BASIC_TOKEN     string
+	DATABASE_URL string
+	GODEV        string
+	PORT         string
+	REDIS_URL    string
 }
 
 var datasetEnv *Environment
@@ -39,17 +32,10 @@ func StartEnv(path string) *Environment {
 	}
 	// godev, _ := strconv.ParseBool(envs["GODEV"])
 	datasetEnv = &Environment{
-		AWS_ACCESS_KEY_ID:      envs["AWS_ACCESS_KEY_ID"],
-		AWS_SECRET_ACCESS_KEY:  envs["AWS_SECRET_ACCESS_KEY"],
-		DATABASE_URL_ANALYTICS: envs["DATABASE_URL_ANALYTICS"],
-		DATABASE_URL:           envs["DATABASE_URL"],
-		TOKEN_API:              envs["OLI_TOKEN"],
-		SECRET_API:             envs["OLI_SECRET"],
-		GODEV:                  envs["GODEV"],
-		PORT:                   port,
-		WHOAMI_URL_DEV:         envs["WHOAMI_URL_DEV"],
-		WHOAMI_URL_PROD:        envs["WHOAMI_URL_PROD"],
-		WHOAMI_BASIC_TOKEN:     envs["WHOAMI_BASIC_TOKEN"],
+		DATABASE_URL: envs["DATABASE_URL"],
+		REDIS_URL:    envs["REDIS_URL"],
+		GODEV:        envs["GODEV"],
+		PORT:         port,
 	}
 	return datasetEnv
 }
